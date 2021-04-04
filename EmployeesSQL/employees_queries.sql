91c3715490d45598c53b7319be4b5b3685c9a8df
Adam Burstyn <adam.burstyn@gmail.com>
2021-04-03 20:10:17 -0400

    added erd and schema

---
+++ EmployeesSQL/employees_queries.sql
@@ -0,0 +1,8 @@
+-- confirm tables are properly loaded
+
+SELECT * FROM titles;
+SELECT * FROM employees;
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
