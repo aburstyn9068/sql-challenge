2078bec0b26e0466670c8cca18fd8469849dc2fd
Adam Burstyn <adam.burstyn@gmail.com>
2021-04-09 21:03:45 -0400

    updated queries

--- EmployeesSQL/employees_queries.sql
+++ EmployeesSQL/employees_queries.sql
@@ -20,7 +20,7 @@ WHERE hire_date LIKE '%/1986';
 -- department name, the manager's employee number, last name, first name.
 SELECT d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name FROM departments d
 JOIN dept_manager dm ON d.dept_no=dm.dept_no
-JOIN employees e ON e.dep_no=dm.emp_no;
+JOIN employees e ON e.emp_no=dm.emp_no;
 
 -- List the department of each employee with the following information: employee number, last name, 
 -- first name, and department name.
